4b2558af5ed3ff3d5c55419012fd5415feda8a49
Isuru Fernando <isuruf@gmail.com>
2019-11-26 20:47:07 -0600

    Use new meson option

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,7 @@ cd build
 rm ../docs/reference/meson.build
 touch ../docs/reference/meson.build 
 
+meson configure -Dvapi=false
 meson --buildtype=release --prefix="$PREFIX" --backend=ninja -Dlibdir=lib ..
 ninja -j${CPU_COUNT}
 ninja install
