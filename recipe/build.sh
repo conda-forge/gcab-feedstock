618fbbb04bdc3ae1e67c761c494d49a6867420b8
Uwe L. Korn <uwe.korn@quantco.com>
2022-01-28 21:26:30 +0100

    Skip introspection generation if cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-mkdir build
+set -euo pipefail
+
+mkdir -p build
 cd build
 
 # need gtkdoc-scan for doc buildin
 rm ../docs/reference/meson.build
 touch ../docs/reference/meson.build 
 
-meson ${MESON_ARGS} --buildtype=release --prefix="$PREFIX" --backend=ninja -Dlibdir=lib -Dvapi=false ..
-ninja -j${CPU_COUNT}
+
+export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+if [[ "${build_platform}" != "${target_platform}" ]]; then
+  # Generation of introspection isn't currently working in cross-compilation mode.
+  export MESON_ARGS="${MESON_ARGS} -Dintrospection=false"
+fi
+meson ${MESON_ARGS:-} --buildtype=release --prefix="$PREFIX" --backend=ninja -Dlibdir=lib -Dvapi=false ..
+ninja
 ninja install
 
